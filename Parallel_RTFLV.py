edee2ed5f02aaf3cc8e6888bdfb2d3e0d4486bf3
Cheney Lin <lincheney@gmail.com>
2012-04-24 21:14:53 +1000

    Now joins files. Filesize, duration are now obtained from metadata. save_stream() uses queues instead of yielding so it continues to run even if main thread pauses.

--- Parallel_RTFLV.py
+++ Parallel_RTFLV.py
@@ -3,13 +3,15 @@
 #       
 #       API to download different segments of a real-time FLV
 #       stream in parallel (if this is possible) and
-#       finally join them (not yet implemented).
+#       finally join them.
 #       
 #       In this way, a 1-hour video that would normally stream
 #       for 1-hour can be downloaded in three 20min downloads
 #       that occur in parallel.
 #
 
+import os
+import shutil
 import urllib2
 import struct
 import Queue
@@ -72,34 +74,48 @@ def set_tag_timestamp (data, timestamp):
     return data[0 : 4] + struct.pack ("!I", timestamp)[1 : ] + data[7 : ]
 
 #
+#       get_metadata_number:
+#       @metadata:      metadata block (body of FLV tag)
+#       @key:           key in metadata
+#
+#       Returns:        the 64-bit float associated with @key
+#                       found in @metadata
+#                       if not found, returns None
+#
+def get_metadata_number (metadata, key):
+    length_str = struct.pack ("!I", len (key) )[2 : ]
+    index = metadata.find (length_str + key)
+    if (index == -1):
+        return None
+    index += len (key) + 3
+    return struct.unpack ("!d", metadata[index : index + 8])[0]
+
+#
 #       save_stream_part:
 #       @url:           URL to open FLV stream
-#       @filename:      base filename - .part6 etc. is appended to this
-#       @duration:      duration to stream (sec)
+#       @filename:      filename to save to
 #       @part:          which number part this is
 #       @inqueue:       #Queue to receive input from
 #       @outqueue:      #Queue to send output to
-#       @report_time:   interval to report stats to @outqueue (msec)
 #
 #       Downloads a part of an FLV stream given in @url to
-#       filename.part(@part) (e.g. john.flv.part3) of the given @duration.
+#       @filename
 #       
 #       The function will return if it receives -1 on @inqueue.
 #       The function will report its status and 0<=progress<=1
 #       via @outqueue with a dict.
+#       status=1 if download finished
+#       status=-1 if download failed
 #
-def save_stream_part (url, filename, part,
-                      inqueue, outqueue):
+def save_stream_part (url, filename, part, inqueue, outqueue):
     stream = urllib2.urlopen (url)
-    
     # non-flv: fail
     if (stream.info ().gettype () != "video/x-flv"):
         stream.close ()
-        outqueue.put ({"part" : part, "status" : "Failed"})
+        outqueue.put ({"part" : part, "status" : -1})
         return
     
-    outfile = open (filename + ".part" + str (part), "w")
-    
+    outfile = open (filename, "w")
     # read the one header ...
     header = read_header (stream)
     if (part == 0):
@@ -111,27 +127,32 @@ def save_stream_part (url, filename, part,
     # end_time specifies the absolute end of this segment
     end_time = float ("inf")
     # duration specifies the relative end of this segment
+    # whenever end_time, offset change, duration must be re-calculated
     duration = (end_time - offset) * 1000
     
     # ... and read all the tags
     for data, _type, timestamp, body in read_tags (stream):
         do_write = True
-        
         if (_type == "\x12"):
             do_write = False
-            
-            # the timeBase in a metadata tag indicates the ACTUAL
-            # starting time of this segment - use this as offset
-            timeBase = body.find ("timeBase")
-            if (timeBase != -1 and timestamp == 0):
-                index = timeBase + len ("timeBase") + 1
-                timeBase = body[index : index + 8]
-                offset = struct.unpack ("!d", timeBase)[0]
-                # new offset -> re-calculate duration
-                duration = (end_time - offset) * 1000
-                # put timeBase - the segment before this one will
-                # need to use it as the end point
-                outqueue.put ({"part" : part, "offset" : offset})
+            # only bother with the initial sets of metadata
+            if (timestamp == 0):
+                if (part == 0):
+                    # for part 0, grab filesize, duration
+                    full_duration = get_metadata_number (body, "duration")
+                    filesize = get_metadata_number (body, "filesize")
+                    
+                    if (full_duration != None and filesize != None):
+                        outqueue.put ({"full-duration" : full_duration,
+                                       "filesize" : filesize})
+                
+                # the timeBase in a metadata tag indicates the ACTUAL
+                # starting time of this segment - use this as offset
+                new_offset = get_metadata_number (body, "timeBase")
+                if (new_offset != None):
+                    offset = new_offset
+                    duration = (end_time - offset) * 1000
+                    outqueue.put ({"part" : part, "offset" : offset})
         elif (_type == "\x08"):
             if ((ord (body[0]) >> 4) == 10):
                 # aac sequence header - only write if first part
@@ -150,12 +171,13 @@ def save_stream_part (url, filename, part,
         if (timestamp >= duration):
             break
         
-        # report progress
-        if (duration == float ("inf") ):
-            progress = 0
-        else:
-            progress = timestamp / duration
-        outqueue.put ({"part" : part, "progress" : progress})
+        # report progress for audio, video tags
+        if (_type == "\x08" or _type == "\x09"):
+            if (duration == float ("inf") ):
+                progress = 0
+            else:
+                progress = timestamp / duration
+            outqueue.put ({"part" : part, "progress" : progress})
         
         try:
             message = inqueue.get_nowait ()
@@ -163,9 +185,9 @@ def save_stream_part (url, filename, part,
                 # we've been told to stop, so fail
                 outfile.close ()
                 stream.close ()
-                outqueue.put ({"part" : part, "status" : "Failed"})
+                outqueue.put ({"part" : part, "status" : -1})
                 return
-            # otherwise, message is a new end_time -> re-calculate duration
+            # otherwise, message is a new end_time
             end_time = message
             duration = (end_time - offset) * 1000
         except Queue.Empty:
@@ -173,40 +195,74 @@ def save_stream_part (url, filename, part,
     # finished reading all that's needed - success!
     outfile.close ()
     stream.close ()
-    outqueue.put ({"part" : part, "status" : "Done"})
+    outqueue.put ({"part" : part, "status" : 1})
 
 #
 #       save_stream:
 #       @url_fn:        function that returns a URL for a given seek-time
 #       @filename:      filename to save FLV to
-#       @duration:      total duration of FLV to download
 #       @parts:         number of parts in which to download FLV
-#       @report_time:   interval to report stats
+#       @mainqueue:     queue to put messages on
+#       @duration:      total duration of FLV to download
 #
 #       Downloads the FLV stream from @url_fn in several parts and save to
-#       @filename. Each part has duration @duration/@parts
-#       If intending to download the whole FLV, it is better to overestimate
-#       @duration.
+#       @filename.
+#       Specify @duration if not downloading full video.
 #       
-#       Yields:         download stats - same as in save_stream_part()
+#       The function can optionally report progress/status to @mainqueue
+#       (in which case it should be threaded). First message is -1 on
+#       on failure, dict with keys "duration", "filesize" on success.
+#       All other tags are same as for save_stream_part(), but
+#       status=2 if ALL parts are finished
+#       
+#       The function will abort if any one part fails.
+#       
+#       If the download was successful, the partial files are joined
+#       into @filename (and then deleted).
 #
-def save_stream (url_fn, filename, duration, parts = 3):
+def save_stream (url_fn, filename, parts = 3,
+                 mainqueue = None, duration = float ("inf")):
     # list of [thread, queue, finished?]
     threads = list ()
     inqueue = Queue.Queue ()
-    part_duration = duration / parts
     
-    for i in range (parts):
+    # start part 0 first to get filesize, duration
+    outqueue = Queue.Queue ()
+    # url for this part
+    part_url = url_fn (0)
+    thread = Thread (target = save_stream_part,
+                     args = (url_fn (0), filename, 0,
+                             outqueue, inqueue) )
+    threads.append ([thread, outqueue, False])
+    thread.daemon = True
+    thread.start ()
+    
+    # expecting first message with filesize, duration; error otherwise
+    message = inqueue.get ()
+    if ("filesize" not in message or "full-duration" not in message):
+        if (mainqueue != None):
+            mainqueue.put (-1)
+        return
+    duration = min (message["full-duration"], duration)
+    if (mainqueue != None):
+        mainqueue.put ({"duration" : duration,
+                        "filesize" : message["filesize"]})
+    
+    # now that we have duration, we can start all other parts
+    part_duration = duration / parts
+    for i in range (1, parts):
         outqueue = Queue.Queue ()
         # url for this part
         part_url = url_fn (i * part_duration)
+        part_filename = filename + ".part" + str (i)
         thread = Thread (target = save_stream_part,
-                         args = (part_url, filename, i,
+                         args = (part_url, part_filename, i,
                                  outqueue, inqueue) )
-        threads.append ([thread, outqueue, False, -1])
+        threads.append ([thread, outqueue, False])
         thread.daemon = True
         thread.start ()
-    threads[-1][1].put (duration)
+    # add 0.5 just in case
+    threads[-1][1].put (duration + 0.5)
     
     while (True):
         message = inqueue.get ()
@@ -214,23 +270,40 @@ def save_stream (url_fn, filename, duration, parts = 3):
         
         if ("status" in message):
             status = message["status"]
-            
             # this part failed, so abort all
-            if (status == "Failed"):
+            if (status == -1):
                 for i in threads:
                     i[1].put (-1)
-                yield message
-                break
+                if (mainqueue != None):
+                    mainqueue.put (message)
+                return
             
             # this part is done, check if all others are done too
-            if (status == "Done"):
+            if (status == 1):
                 threads[part][2] = True
                 if (all (x[2] for x in threads) ):
-                    yield message
+                    message["status"] = 2
+                    if (mainqueue != None):
+                        mainqueue.put (message)
                     break
         
         if ("offset" in message):
             # got an offset for part: this is end_time for part-1
             if (part > 0):
                 threads[part - 1][1].put (message["offset"])
-        yield message
+        if (mainqueue != None):
+            mainqueue.put (message)
+    
+    # join all files and delete partials
+    # first part is contained in @filename
+    # others in @filename.partX
+    ofile = open (filename, "a")
+    for i in range (1, parts):
+        part_filename = filename + ".part" + str (i)
+        
+        partfile = open (part_filename, "r")
+        shutil.copyfileobj (partfile, ofile)
+        partfile.close ()
+        
+        os.remove (part_filename)
+    ofile.close ()
--- example.py
+++ example.py
@@ -4,13 +4,11 @@
 #       Example command line program making use of
 #       Parallel_RTFLV
 #       
-#       Usage: python example.py url outfile duration parts
+#       Usage: python example.py url outfile parts
 #       
 #       url:            url of FLV stream - where seeking is done
 #                       by appending &seek=123
 #       outfile:        filename to save to
-#       duration:       duration of video to download (or estimate;
-#                       if estimate, overestimate is better)
 #       parts:          number of parts to split up downloading
 #
 #       Simple statistics are printed out in a 'tabular' format
@@ -18,35 +16,58 @@
 #
 
 import sys
-import shutil
+from threading import Thread
+import Queue
 from Parallel_RTFLV import save_stream
 
-if (len (sys.argv) < 3):
-    print "Usage: python {} url outfile duration parts".format (sys.argv[0])
+if (len (sys.argv) < 2):
+    print "Usage: python {} url outfile parts".format (sys.argv[0])
     sys.exit (0)
 
-url, outfile, duration, parts = sys.argv[1 : 5]
-duration = int (duration)
+url, outfile, parts = sys.argv[1 : 4]
 parts = int (parts)
-
+# function to make url
 url_fn = lambda time: url + "&seek=" + str (time)
 
+# thread the function
+mainqueue = Queue.Queue ()
+thread = Thread (target = save_stream,
+                 args = (url_fn, outfile, parts, mainqueue) )
+thread.daemon = True
+thread.start ()
+
+# expecting -1 for failure, otherwise dict with filesize, duration
+message = mainqueue.get ()
+if (message == -1):
+    print "Download failed"
+    sys.exit (1)
+print message
+
+# running stats for each part
+progress = ["{:<9}".format (0)] * parts
 # header for stats
 for i in range (parts):
     print "Part {:2}  ".format (i),
 print
 
-# running stats for each part
-progress = ["{:<9}".format (0)] * parts
-
-for message in save_stream (url_fn, outfile, duration, parts):
+while (True):
+    message = mainqueue.get ()
     part = message["part"]
     if ("status" in message):
         status = message["status"]
-        progress[part] = "{:<9}".format (status)
         
-        if (status == "Failed"):
+        if (status == -1):
             print "\nDownload of part {} failed. Aborting...".format (part)
+            progress[part] = "{:<9}".format ("Failed")
+            break
+        elif (status == 1):
+            progress[part] = "{:<9}".format ("Done")
+        elif (status == 2):
+            # all parts finished; waiting to join files
+            progress[part] = "{:<9}".format ("Done")
+            print "\r" + " ".join (progress)
+            print "Joining files..."
+            break
 
     if ("progress" in message):
         progress[part] = "{:<9.2f}".format (message["progress"] * 100)
@@ -54,3 +75,6 @@ for message in save_stream (url_fn, outfile, duration, parts):
     # print out the stats
     print "\r" + " ".join (progress),
     sys.stdout.flush ()
+
+# wait for thread to finish
+thread.join ()
